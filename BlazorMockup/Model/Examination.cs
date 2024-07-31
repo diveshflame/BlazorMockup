55cf3060f11e21db1e3a70c2035b1f78c13f7085
Divesh Shetty <divsh@eg.dk>
2024-07-31 15:58:39 +0530

    Created Model classes

---
+++ BlazorMockup/Model/Examination.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorMockup.Model
+{
+    public class Examination
+    {
+        public DateTime Date { get; set; }
+        public string ExaminationType { get; set; }
+        public double Weight { get; set; }
+        public double HeadCircumference { get; set; }
+        public double Height { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ BlazorMockup/Model/Vaccination.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorMockup.Model
+{
+    public class Vaccination
+    {
+        public DateTime Date { get; set; }
+        public string Vaccine { get; set; }
+        public string Dosage { get; set; }
+        public string Duration { get; set; }
+        public string Note { get; set; }
+        public string Credebility { get; set; }
+    }
+}
