a4a14f56e4ef81311307999602dd669b99af80ae
Divesh Shetty <divsh@eg.dk>
2024-08-06 16:11:21 +0530

    Refactored Datastore code

--- BlazorMockup/DataWarehouse/DataStore.cs
+++ BlazorMockup/DataWarehouse/DataStore.cs
@@ -8,140 +8,44 @@ namespace BlazorMockup.DataWarehouse
 
         public DataStore()
         {
-            Examinations = new List<Examination>();
-
-            // Add some sample data
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now.AddDays(-10),
-                ExaminationType = "Week",
-                Weight = 70.5,
-                HeadCircumference = 55.2,
-                Height = 175.5,
-                Note = "Sample note 1"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now.AddDays(-12),
-                ExaminationType = "Month",
-                Weight = 70.8,
-                HeadCircumference = 55.8,
-                Height = 175.8,
-                Note = "Sample note 5"
-            });
-            
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now,
-                ExaminationType = "Quarter",
-                Weight = 74.5,
-                HeadCircumference = 59.5,
-                Height = 180.5,
-                Note = "Sample note 13"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now.AddDays(-5),
-                ExaminationType = "Week",
-                Weight = 71.2,
-                HeadCircumference = 56.1,
-                Height = 176.3,
-                Note = "Sample note 2"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now,
-                ExaminationType = "Week",
-                Weight = 72.1,
-                HeadCircumference = 57.5,
-                Height = 177.2,
-                Note = "Sample note 3"
-            });
-
-              Examinations.Add(new Examination
-            {
-                Date = DateTime.Now.AddDays(-15),
-                ExaminationType = "Week",
-                Weight = 69.8,
-                HeadCircumference = 54.5,
-                Height = 174.8,
-                Note = "Sample note 4"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now.AddDays(-8),
-                ExaminationType = "Month",
-                Weight = 71.5,
-                HeadCircumference = 56.5,
-                Height = 176.5,
-                Note = "Sample note 6"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now.AddDays(-3),
-                ExaminationType = "Month",
-                Weight = 72.3,
-                HeadCircumference = 57.2,
-                Height = 177.5,
-                Note = "Sample note 7"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now.AddDays(-2),
-                ExaminationType = "Month",
-                Weight = 72.8,
-                HeadCircumference = 57.8,
-                Height = 178.2,
-                Note = "Sample note 8"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now.AddDays(-1),
-                ExaminationType = "Year",
-                Weight = 73.2,
-                HeadCircumference = 58.2,
-                Height = 178.5,
-                Note = "Sample note 9"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now,
-                ExaminationType = "Year",
-                Weight = 73.5,
-                HeadCircumference = 58.5,
-                Height = 179.2,
-                Note = "Sample note 10"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now,
-                ExaminationType = "Year",
-                Weight = 73.8,
-                HeadCircumference = 58.8,
-                Height = 179.5,
-                Note = "Sample note 11"
-            });
-
-            Examinations.Add(new Examination
-            {
-                Date = DateTime.Now,
-                ExaminationType = "Quarter",
-                Weight = 74.2,
-                HeadCircumference = 59.2,
-                Height = 180.2,
-                Note = "Sample note 12"
-            });
-
-
+            Examinations = CreateExaminations();
         }
+
+        private List<Examination> CreateExaminations()
+        {
+            var examinations = new List<Examination>();
+
+            // Generate examination types
+            var examinationTypes = new[] { "Week", "Month", "Quarter", "Year" };
+            var dates = Enumerable.Range(0, 16).Select(i => DateTime.Now.AddDays(-10 * (i + 1))).ToArray();
+            var weights = GenerateValues(16, 70.0, 77.5, 1.3);
+            var headCircumferences = GenerateValues(16, 55.0, 62.5, 1.3);
+            var heights = GenerateValues(16, 175.0, 183.5, 1.3);
+            var notes = Enumerable.Range(1, 16).Select(i => $"Sample note {i}").ToArray();
+
+            for (int i = 0; i < examinationTypes.Length; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    examinations.Add(new Examination
+                    {
+                        Date = dates[i * 4 + j],
+                        ExaminationType = examinationTypes[j],
+                        Weight = weights[i * 4 + j],
+                        HeadCircumference = headCircumferences[i * 4 + j],
+                        Height = heights[i * 4 + j],
+                        Note = notes[i * 4 + j]
+                    });
+                }
+            }
+
+            return examinations;
+        }
+
+        private double[] GenerateValues(int count, double start, double end, double increment)
+        {
+            return Enumerable.Range(0, count).Select(i => start + i * increment).ToArray();
+        }
+
     }
 }
