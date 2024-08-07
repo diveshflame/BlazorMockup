0bb0115d7e8dc1c3bd441b076544012fde48baa5
Divesh Shetty <divsh@eg.dk>
2024-08-07 16:36:29 +0530

    Enabled toggle of Oversigt and Graphs

---
+++ BlazorMockup/ViewModel/GraphToggleBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorMockup;
+
+public class GraphToggleBase :ComponentBase
+{
+    public bool isGridActive = true;
+
+        [Parameter] public EventCallback<bool> OnToggleChanged { get; set; }
+
+        public string GridClass => isGridActive ? "bg-[#32947C] text-white" : "bg-white text-black";
+        public string GraphClass => isGridActive ? "bg-white text-black" : "bg-[#32947C] text-white";
+
+        public async Task ToggleGrid()
+        {
+            isGridActive = true;
+            await OnToggleChanged.InvokeAsync(isGridActive);
+        }
+
+        public async Task ToggleGraph()
+        {
+            isGridActive = false;
+            await OnToggleChanged.InvokeAsync(isGridActive);
+        }
+
+}
